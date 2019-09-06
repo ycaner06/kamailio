e83c5ae45f7ea520b9f305fdd65c7b235fca46b8
Yasin CANER <caner_yaso@hotmail.com>
2019-09-06 06:36:31 +0000

    b2bua : 200 Ok donmesi icin calisiliyor 1

--- src/modules/b2bua/b2bua_mod.c
+++ src/modules/b2bua/b2bua_mod.c
@@ -50,6 +50,7 @@
 #include "../../core/parser/parse_uri.h"
 #include "../../core/parser/parse_to.h"
 #include "../../core/parser/parse_from.h"
+#include "../../core/data_lump.h"
 
 #include "../../modules/sanity/api.h"
 #include "../../lib/ims/useful_defs.h"
@@ -85,6 +86,10 @@ static int w_b2bua_send(sip_msg_t *msg);
 static int b2b_msg_received(sr_event_param_t *evp);
 void b2b_response_cb(struct cell* t, int type, struct tmcb_params* ps);
 struct depo *get_root(void);
+int recollect_via(sip_msg_t *msg, str *via_body);
+int b2b_sip_rw(char *s, int len);
+int b2b_prepare_msg(sip_msg_t *msg);
+char* b2b_msg_update(sip_msg_t *msg, unsigned int *olen);
 
 static param_export_t params[]={
 	{"mask_key",		PARAM_STR, &b2b_mask_key},
@@ -120,18 +125,20 @@ struct module_exports exports= {
 typedef struct depo {
 	str callid;
 	str fromtag;
+	str via0;
+
 	struct depo *next;
 } depo_t;
 
 /**
  * init module function
  */
+#ifndef WITH_DEPOCUK
+#define WITH_DEPOCUK
 
-#ifndef WITH_DEPO
-#define WITH_DEPO
 depo_t *depocuk=NULL;
- int xy =0;
 #endif
+
 
 static int mod_init(void)
 {
@@ -144,9 +151,6 @@ static int mod_init(void)
 		LM_ERR("can't load TM API\n");
 		return -1;
 	}
-LM_ERR("MODINIT\n");
-	//depocuk = (struct depo *)pkg_malloc(sizeof(struct depo ));
-	//memset(depocuk,'0',sizeof(depo_t));
 
 	sr_event_register_cb(SREV_NET_DATA_IN,  b2b_msg_received);
 
@@ -161,11 +165,10 @@ static int w_b2bua_send(sip_msg_t *msg)
 	str method = str_init("INVITE");
 	str to_header = str_init("sip:deneme@192.168.1.39");
 	str from_header = str_init("sip:yc@192.168.1.39");
-	str next_hop = str_init("sip:192.168.1.39:5062");
+	//str next_hop = str_init("sip:192.168.1.39:5062");
 	str ruri = str_init("sip:123@192.168.1.39:5062");
 	struct depo *mydepo;
 
-	int *c;
 
 	LM_ERR("Giriş ok\r\n");
 	if (parse_msg(msg->buf, msg->len, msg)!=0)
@@ -235,19 +238,21 @@ static int w_b2bua_send(sip_msg_t *msg)
 		LM_ERR("FROM TAG %.*s \r\n",from_b->tag_value.len,from_b->tag_value.s);
 	}
 
-	mydepo = (struct depo *)pkg_malloc(sizeof(struct depo));
+	mydepo = (struct depo *)shm_malloc(sizeof(struct depo));
 
 	if(mydepo == 0) {
-		PKG_MEM_ERROR;
-		return 1;
+		SHM_MEM_ERROR;
+		shm_free(mydepo);
+		return -1;
 	}
 
 	memset(mydepo,'0',sizeof(depo_t));
 
-	LM_ERR("11111 \r\n");
+	LM_ERR("11111 VIA [%.*s]\r\n",msg->h_via1->body.len,msg->h_via1->body.s);
 
-	mydepo->callid.s =(char *)pkg_malloc(msg->callid->body.len);
-	mydepo->fromtag.s =(char *)pkg_malloc(from_b->tag_value.len);
+	mydepo->callid.s =(char *)shm_malloc(msg->callid->body.len);
+	mydepo->fromtag.s =(char *)shm_malloc(from_b->tag_value.len);
+	mydepo->via0.s =(char *)shm_malloc(msg->h_via1->body.len);
 	/*mydepo->callid.s =msg->callid->body.s;
 	mydepo->callid.len =msg->callid->body.len;
 */
@@ -265,22 +270,19 @@ if(mydepo->callid.s){
 	memcpy(mydepo->fromtag.s,from_b->tag_value.s,from_b->tag_value.len);
 	LM_ERR("33333 \r\n");
 
+
+	memcpy(mydepo->via0.s,msg->h_via1->body.s,msg->h_via1->body.len);
+	mydepo->via0.len = msg->h_via1->body.len;
+	mydepo->fromtag.len=from_b->tag_value.len;
+	mydepo->callid.len = msg->callid->body.len;
 	mydepo->next = depocuk;
+
 	LM_ERR("444444 \r\n");
 
-
-
 	depocuk = mydepo;
+
 	LM_ERR("55555 %p \r\n",mydepo);
 	LM_ERR("66666 %p \r\n",depocuk);
-	c = &xy;
-	*c=20;
-	LM_ERR("7777777 %p \r\n",&xy);
-	LM_ERR("8888888 %d \r\n",xy);
-	LM_ERR("9999999 %d \r\n",*c);
-	LM_ERR("5413123 %p \r\n",c);
-
-
 
 
   if(msg->first_line.type==SIP_REQUEST) {
@@ -316,7 +318,7 @@ if(mydepo->callid.s){
 }
 
 void b2b_response_cb(struct cell* t, int type, struct tmcb_params* ps){
-
+/*
 	struct depo *dep = get_root();
 	LM_ERR("b2b response cb \r\n");
 	LM_ERR("11111 %p\r\n",dep);
@@ -327,7 +329,7 @@ void b2b_response_cb(struct cell* t, int type, struct tmcb_params* ps){
 
 			LM_ERR("Depocuk %.*s\r\n",dep->callid.len,dep->callid.s);}
 		}
-
+*/
 
 }
 
@@ -340,27 +342,190 @@ static int b2b_msg_received(sr_event_param_t *evp)
 {
 	sip_msg_t msg;
 	str *obuf;
-	char *nbuf = NULL;
-	int direction;
-	int dialog;
+  char *nbuf = NULL;
+	struct ip_addr hope;
+	struct depo *dep = depocuk;
+	str hopestr = str_init("192.168.1.39");
 
-	struct depo *dep = get_root();
-	LM_ERR("RECEIVED %d\r\n",xy);
-	LM_ERR("7777777 %p \r\n",&xy);
+
+	if(str2ipbuf(&hopestr,&hope)<0){
+				LM_ERR("Couldn't get sdp c line IP address\n");
+				return -1;
+	}
+
+	if(!ip_addr_cmp(&evp->rcv->src_ip,&hope)){
+		return -1;
+	}
+
+	LM_ERR("11111 %p\r\n",dep);
 
 
 	obuf = (str*)evp->data;
 	memset(&msg, 0, sizeof(sip_msg_t));
 	msg.buf = obuf->s;
 	msg.len = obuf->len;
-	LM_ERR("11111 %p\r\n",dep);
+
+	if(b2b_prepare_msg(&msg)!=0){
+			LM_ERR("b2b_prepare_msg ERROR   \r\n");
+			return -1;
+	}
+
 	if(dep){
 		LM_ERR("22222 %p\r\n",dep);
 		if(dep->callid.len>0 && dep->callid.s){
 			LM_ERR("333333 %p \r\n",dep->callid.s);
 
-			LM_ERR("Depocuk %.*s\r\n",dep->callid.len,dep->callid.s);}
+			LM_ERR("Depocuk [%.*s][%d]\r\n",dep->callid.len,dep->callid.s,dep->callid.len);
+			LM_ERR("VIA [%.*s][%d]\r\n",dep->via0.len,dep->via0.s,dep->via0.len);
+			}
 		}
+		if(msg.first_line.type==SIP_REQUEST)
+		{
+				return 0;
 
+		}else{ //reply
+
+			int sonuc = recollect_via(&msg,&dep->via0);
+			LM_ERR("IT IS A REPLY[%d] \r\n",sonuc);
+
+			nbuf = b2b_msg_update(&msg, (unsigned int*)&obuf->len);
+			if(!nbuf==NULL){
+				LM_ERR("NBUF [%s]\r\n",nbuf);
+			} 
+
+			if(obuf->len>=BUF_SIZE)
+			{
+				LM_ERR("new buffer overflow (%d)\n", obuf->len);
+				return -1;
+			}
+			memcpy(obuf->s, nbuf, obuf->len);
+			obuf->s[obuf->len] = '\0';
+
+		}
 return 1;
+}
+
+
+char* b2b_msg_update(sip_msg_t *msg, unsigned int *olen)
+{
+	struct dest_info dst;
+
+	init_dest_info(&dst);
+	dst.proto = PROTO_UDP;
+	return build_req_buf_from_sip_req(msg,
+			olen, &dst, BUILD_NO_LOCAL_VIA|BUILD_NO_VIA1_UPDATE);
+}
+
+
+
+int recollect_via(sip_msg_t *msg, str *via_body)
+{
+	hdr_field_t *hdr;
+	struct via_body *via;
+	struct lump* l;
+	int i;
+	str out;
+	int vlen;
+
+	i=0;
+	for(hdr=msg->h_via1; hdr; hdr=next_sibling_hdr(hdr))
+	{
+		for(via=(struct via_body*)hdr->parsed; via; via=via->next)
+		{
+				i++;
+				vlen = b2b_sip_rw(via->name.s, via->bsize);
+				l=del_lump(msg, via->name.s-msg->buf, vlen, 0);
+				if (l==0)
+				{
+					LM_ERR("failed deleting via [%d]\n", i);
+					return -1;
+				}
+				if (insert_new_lump_after(l, via_body->s, via_body->len, 0)==0)
+				{
+					LM_ERR("could not insert new lump\n");
+					return -1;
+				}
+			}
+	}
+
+	return 0;
+}
+
+int b2b_sip_rw(char *s, int len)
+{
+	while(len>0)
+	{
+		if(s[len-1]==' ' || s[len-1]=='\t' || s[len-1]=='\n' || s[len-1]=='\r'
+				|| s[len-1]==',')
+			len--;
+		else return len;
+	}
+	return 0;
+}
+
+
+
+/**
+ *
+ */
+
+int b2b_prepare_msg(sip_msg_t *msg)
+{
+	if (parse_msg(msg->buf, msg->len, msg)!=0)
+	{
+		LM_DBG("outbuf buffer parsing failed!");
+		return 1;
+	}
+
+	if(msg->first_line.type==SIP_REQUEST)
+	{
+		if(!IS_SIP(msg))
+		{
+			LM_DBG("non sip request message\n");
+			return 1;
+		}
+	} else if(msg->first_line.type!=SIP_REPLY) {
+		LM_DBG("non sip message\n");
+		return 1;
+	}
+
+	if (parse_headers(msg, HDR_EOH_F, 0)==-1)
+	{
+		LM_DBG("parsing headers failed [[%.*s]]\n",
+				msg->len, msg->buf);
+		return 2;
+	}
+
+	/* force 2nd via parsing here - it helps checking it later */
+	if (parse_headers(msg, HDR_VIA2_F, 0)==-1
+		|| (msg->via2==0) || (msg->via2->error!=PARSE_OK))
+	{
+		LM_DBG("no second via in this message \n");
+	}
+
+	if(parse_from_header(msg)<0)
+	{
+		LM_ERR("cannot parse FROM header\n");
+		return 3;
+	}
+
+	if(parse_to_header(msg)<0 || msg->to==NULL)
+	{
+		LM_ERR("cannot parse TO header\n");
+		return 3;
+	}
+
+	if(get_to(msg)==NULL)
+	{
+		LM_ERR("cannot get TO header\n");
+		return 3;
+	}
+
+	if(msg->via1==NULL || msg->callid==NULL) {
+		LM_ERR("mandatory headers missing - via1: %p callid: %p\n",
+				msg->via1, msg->callid);
+		return 4;
+	}
+
+	return 0;
 }
